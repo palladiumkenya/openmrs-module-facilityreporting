6318fc678646f5fb5e8c66721316c26ce990a9ad
makombe <kennedymakombe@gmail.com>
2020-04-01 10:57:00 +0300

    Allow for editing of dataset indicators

--- omod/src/main/java/org/openmrs/module/facilityreporting/page/controller/NewReportIndicatorFormPageController.java
+++ omod/src/main/java/org/openmrs/module/facilityreporting/page/controller/NewReportIndicatorFormPageController.java
@@ -1,15 +1,17 @@
 package org.openmrs.module.facilityreporting.page.controller;
 
 import org.openmrs.module.facilityreporting.api.models.FacilityReportDataset;
+import org.openmrs.module.facilityreporting.api.models.FacilityReportIndicator;
 import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class NewReportIndicatorFormPageController {
 	
 	public void controller(@RequestParam("returnUrl") String url, @RequestParam("datasetId") FacilityReportDataset dataset,
-	        PageModel model) {
+	        @RequestParam(value = "id", required = false) FacilityReportIndicator facilityReportIndicator, PageModel model) {
 		model.addAttribute("returnUrl", url);
 		model.addAttribute("dataset", dataset);
+		model.addAttribute("facilityReportIndicator", facilityReportIndicator);
 		
 	}
 }
